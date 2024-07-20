2da6c76039fde8479f81256f2fae55e27f1dcc91
A-Walrus <ofek.r.guy@gmail.com>
2024-07-20 16:47:05 +0300

    Add column

--- src/AniMonad/Element/Layout.hs
+++ src/AniMonad/Element/Layout.hs
@@ -1,8 +1,8 @@
-
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
-module AniMonad.Element.Layout where
-import AniMonad.Element.Base
 
+module AniMonad.Element.Layout where
+
+import AniMonad.Element.Base
 
 row :: (Element e) => Float -> [e] -> [Transformed e]
 row gap elements = zipWith move starts elements
@@ -15,3 +15,15 @@ row gap elements = zipWith move starts elements
     move x e = at (V2 (x - elem_x) 0) e
       where
         (BoundingBox (V2 elem_x _) _) = box e
+
+column :: (Element e) => Float -> [e] -> [Transformed e]
+column gap elements = zipWith move starts elements
+  where
+    num_gaps = length elements - 1
+    heights = map (boxHeight . box) elements
+    total_height = (fromIntegral num_gaps * gap) + sum heights
+    start = -(total_height / 2)
+    starts = scanl (\y h -> y + h + gap) start heights
+    move y e = at (V2 0 (y - elem_y)) e
+      where
+        (BoundingBox (V2 _ elem_y) _) = box e
